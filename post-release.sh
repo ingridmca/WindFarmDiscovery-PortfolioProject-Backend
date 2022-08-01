159cf478515dc1910cdc1d5b6d49c63aeae46deb
Ingrid Almeida <ingridmcalmeida@gmail.com>
2022-08-01 17:10:39 +0200

    deploy seeding DB

--- post-release.sh
+++ post-release.sh
@@ -1,1 +1,3 @@
 npx sequelize-cli db:migrate
+node scripts/generatePerformenceData.js 
+npx sequelize-cli db:seed:all
--- scripts/generatePerformenceData.js
+++ scripts/generatePerformenceData.js
@@ -13,7 +13,7 @@ const turbineIds = async () => {
   for (const turbine of turbines) {
     //    break;
     const start = new Date("2022-01-03 01:00:00");
-    const end = new Date("2022-04-01 00:00:00");
+    const end = new Date("2022-01-20 00:00:00");
     const oneHourMs = 10 * 60 * 1000 * 6; // ms
 
     let batchTurbines = [];
